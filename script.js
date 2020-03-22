b08250e713d08f3bcbb871057505a0ef3c9eee90
Devin Heigert <heigertd@gmail.com>
2020-03-22 16:24:47 -0700

    made todo list

---
+++ script.js
@@ -0,0 +1,6 @@
+//TODO: look through weather api to see how it works
+//TODO: collect city from user input search bar
+//TODO: use an ajax method to call api and get the info from that city
+//TODO: collect the nessacary info from the api (5 day forcast, current temp, current weather etc.)
+//TODO: use local storage to save the latest results
+//TODO: display it back on the screen 
